fe9f66a2043cbc3c0f815a600de3d52fa150492f
orlando <orlandoca647@gmail.com>
2018-04-14 18:54:02 -0400

    - ARREGLANDO LOS ATRIBUTOS DEL REPORTE OSEA LAS DIMENSIONES - AGREGANDO METODO PARA CONVERTIR EN LETRAS - MODIFICANDO EN LA LISTA DE CAJAS QUE APAREZCA EL USUARIO EN VEZ DEL ID - DANDO ESTILO AL BOTON IMPRIMIR RECIBO EN MODULO RECIBOS

--- app/pdf/reporte_pdf.rb
+++ app/pdf/reporte_pdf.rb
@@ -6,6 +6,7 @@ class ReportePdf < Prawn::Document
     text_content
     table_content
     table_content2
+    table_context3
 
   end
 
@@ -27,24 +28,24 @@ class ReportePdf < Prawn::Document
     #   text "#{@factura.nro_fac}"
     # end
     bounding_box([0, 600], :width => 1200, :height => 100) do
-      text "Fecha: #{@recibo.created_at.strftime('%m/%d/%Y')}", size: 10, style: :bold
+      text "FECHA: #{@recibo.created_at.strftime('%d/%m/%Y')}", size: 15, style: :bold
     end
 
     bounding_box([0, 580], :width => 300, :height => 300) do
-      text "Nombre: #{@recibo.contrato.inquilino.nombre} #{@recibo.contrato.inquilino.apellido}", size: 10, style: :bold
+      text "NOMBRE DEL INQUILINO: #{@recibo.contrato.inquilino.nombre} #{@recibo.contrato.inquilino.apellido}", size: 15, style: :bold
      end
 
-     
+
 
     move_down -420
     bounding_box([0, cursor], :width => 290, :height => 90) do
        transparent(0.5) { stroke_bounds }
        move_down 10
        indent(20) do
-        text "Colegio Parroquial Privado San Pio X", size: 20, style: :bold
+        text " #{@recibo.contrato.departamento.edificio.nombre_edificio }", size: 20, style: :bold
        end
        indent(30) do
-         text "Padre Jose Kreusser Nº306 e/Juan Leon Mallorquin", size: 10
+         text " #{@recibo.contrato.departamento.edificio.direccion }", size: 15
          text "Telef.:(071)203778 Encarnacion - Paraguay", size: 10
          # stroke_horizontal_rule
        end
@@ -55,18 +56,13 @@ class ReportePdf < Prawn::Document
        transparent(0.5) { stroke_bounds }
        move_down 10
        indent(20) do
-        text "Timbrado Nº XXXXXXXX", size: 10, style: :bold
-        text "RUC: 80056885-0", size: 15, style: :bold
-        text "Inicio Vigencia: 05/setiembre/2016", size: 8
-        text "Fin de Vigencia: 30/setiembre/2017", size: 8
-        text "FACTURA Nº", size: 15, style: :bold
-        text "#{@recibo.id}", size: 10
+        text "RECIBO ", size: 20, style: :bold
+        text "Nº 001-001-00#{@recibo.id}", size: 15, style: :bold
        end
     end
-     
+
 
   end
-
 
   def table_content
     # This makes a call to product_rows and gets back an array of data that will populate the columns and rows of a table
@@ -76,28 +72,24 @@ class ReportePdf < Prawn::Document
       row(0).font_style = :bold
       self.header = true
       self.row_colors = ['DDDDDD', 'FFFFFF']
-      self.column_widths = [40, 300, 200]
+      self.column_widths = [100, 240, 200]
     end
   end
   
   def detalles_rows
     move_down 45
-    [['Numero de Cuotaº', 'Descripcion', 'total']]+
+    [['NUMERO DE CUOTAº', 'DESCRIPCION', 'PRECIO UNITARIO']]+
         @recibo.detalle_recibos.map do |detalle|
-        [detalle.cuota, detalle.descripcion, detalle.total]
+        [detalle.cuota, detalle.descripcion + " DEL DEPARTAMENTO #{@recibo.contrato.departamento.nombre_departamento}" , detalle.total]
       end
   end
 
-
   def table_content2
-    # This makes a call to product_rows and gets back an array of data that will populate the columns and rows of a table
-    # I then included some styling to include a header and make its text bold. I made the row background colors alternate between grey and white
-    # Then I set the table column widths
     table detalles_rows2 do
       row(0).font_style = :bold
       self.header = true
       self.row_colors = ['DDDDDD', 'FFFFFF']
-      self.column_widths = [340, 200]
+      self.column_widths = [120, 420]
     end
   end
   
@@ -107,7 +99,25 @@ class ReportePdf < Prawn::Document
 
     @recibo.detalle_recibos.each {|detalle| @total += detalle.total}
 
-    [['TOTAL ',  @total]]
+    [['TOTAL ',  @total ]]
+
   end
 
+  def table_context3
+    table detalles_rows3 do
+      row(0).font_style = :bold
+      self.header = true
+      self.row_colors = ['DDDDDD', 'FFFFFF']
+      self.column_widths = [200,340]
+    end
+  end
+
+  def detalles_rows3
+    move_down 40
+    @total=0
+
+    @recibo.detalle_recibos.each {|detalle| @total += detalle.total}
+
+    [['TOTAL A PAGAR EN GUARANIES:  ', @total.to_words.upcase ]]
+  end
 end
