fce0808f01ff514e0a68318c0bb5b06bec275d88
orlando <orlandoca647@gmail.com>
2018-05-09 21:38:32 -0400

    - dando estilo al modulo gastos - validacion en el modelo y vista - diseñando el total de los detalles - creando script para la suma

--- app/models/gasto.rb
+++ app/models/gasto.rb
@@ -1,2 +1,11 @@
 class Gasto < ActiveRecord::Base
+  belongs_to :edificio
+
+  validates :detalle, presence: true
+
+  validates :costo, presence: true
+  validates :costo, numericality: true
+  validates :costo, length: { maximum: 10 }
+
+
 end
